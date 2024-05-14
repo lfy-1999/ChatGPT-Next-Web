21ae411828678a4cb7ff55b64386bfeae355ca1f
lfy-1999 <131640436+lfy-1999@users.noreply.github.com>
2024-05-14 16:25:12 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          <a href="https://www.lfy1999.com" target="_blank">LFY1999</a>
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          LFY&apos;s AI assistant.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
@@ -217,7 +217,7 @@ export function SideBar(props: { className?: string }) {
             </Link>
           </div>
           <div className={styles["sidebar-action"]}>
-            <a href={REPO_URL} target="_blank" rel="noopener noreferrer">
+            <a href="https://www.lfy1999.com/nas/" target="_blank">
               <IconButton icon={<GithubIcon />} shadow />
             </a>
           </div>
